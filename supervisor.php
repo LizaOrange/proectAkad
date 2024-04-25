58651e2eaaa9403ea4d25fe96cc732741030310a
alisaorange <kolosovaalisa@gmail.com>
2024-04-25 12:38:28 +0300

    новый фавикон добавили хэдер в каждую страницу

--- supervisor.php
+++ supervisor.php
@@ -1,4 +1,20 @@
-<?php include 'head.php'; ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="Image/x-icon" href="/images/favicon.ico" rel="icon">
+    <title>Супервайзер службы према и размещения</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="css/select2.min.css">
+</head>
+<body>
+<div class="wrapper">
 <?php include 'header.php'; ?>
 <main>
     <section class="home home--admin">
